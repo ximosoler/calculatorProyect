c17f80654805d81994f45b47de18927e3f7ca2f8
ximosoler <xsoler98@gmail.com>
2022-09-28 23:38:23 +0200

    calculator

---
+++ src/main/java/net/ausiasmarch/calculator/api/CalculatorController.java
@@ -0,0 +1,48 @@
+package net.ausiasmarch.calculator.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.ausiasmarch.calculator.entity.Calculation;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+    
+    @GetMapping("/add/{op1}/{op2}")
+    public ResponseEntity<Calculation> add(@PathVariable(value = "op1") int op1,
+                                            @PathVariable(value = "op2") int op2) {
+        Calculation oCalc = new Calculation(op1, op2);
+        oCalc.setResult(op1+op2);
+        return new ResponseEntity<>(oCalc, HttpStatus.OK);
+    }
+
+    @GetMapping("/minus/{op1}/{op2}")
+    public ResponseEntity<Calculation> minus(@PathVariable(value = "op1") int op1,
+                                            @PathVariable(value = "op2") int op2) {
+        Calculation oCalc = new Calculation(op1, op2);
+        oCalc.setResult(op1-op2);
+        return new ResponseEntity<>(oCalc, HttpStatus.OK);
+    }
+
+    @GetMapping("/multiply/{op1}/{op2}")
+    public ResponseEntity<Calculation> multiply(@PathVariable(value = "op1") int op1,
+                                        @PathVariable(value = "op2") int op2) {
+        Calculation oCalc = new Calculation(op1, op2);
+        oCalc.setResult(op1*op2);
+        return new ResponseEntity<>(oCalc, HttpStatus.OK);
+    }
+
+    @GetMapping("/div/{op1}/{op2}")
+    public ResponseEntity<Calculation> div(@PathVariable(value = "op1") int op1,
+                                        @PathVariable(value = "op2") int op2) {
+       Calculation oCalc = new Calculation(op1, op2);
+        oCalc.setResult(op1/op2);
+        return new ResponseEntity<>(oCalc, HttpStatus.OK);
+
+    }
+}
---
+++ src/main/java/net/ausiasmarch/calculator/entity/Calculation.java
@@ -0,0 +1,33 @@
+package net.ausiasmarch.calculator.entity;
+
+public class Calculation {
+    
+    private int op1;
+    private int op2;
+    private int result;
+
+    public Calculation(int op1, int op2) {
+        this.op1 = op1;
+        this.op2 = op2;
+    }
+
+    public int getOp1(){
+        return this.op1;
+    }
+
+    public int getOp2(){
+        return this.op2;
+    }
+    public int getResult(){
+        return this.result;
+    }
+    public void setOp1(int op1){
+        this.op1 = op1;
+    }
+    public void setOp2(int op2){
+        this.op2 = op2;
+    }
+    public void setResult(int string){
+        this.result = string;
+    }
+}
