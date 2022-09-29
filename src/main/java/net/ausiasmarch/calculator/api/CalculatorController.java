679b9ba4cfd8285bffc8e8cf4f0ec1d68838e336
ximosoler <xsoler98@gmail.com>
2022-09-30 01:12:37 +0200

    postman

--- src/main/java/net/ausiasmarch/calculator/api/CalculatorController.java
+++ src/main/java/net/ausiasmarch/calculator/api/CalculatorController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,5 +46,11 @@ public class CalculatorController {
         oCalc.setResult(op1/op2);
         return new ResponseEntity<>(oCalc, HttpStatus.OK);
 
+    }
+
+    @PostMapping
+    public ResponseEntity<Calc>calculadora(@RequestBody Calc oCalc){
+        oCalc.setResult(oCalc.getOp1() + oCalc.getOp2());
+        return new ResponseEntity<Calc>(oCalc, HttpStatus.OK);
     }
 }
